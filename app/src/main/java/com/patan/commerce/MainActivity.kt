33328988b033286a78dbafdcc4bd1451a9393209
GokalpPATAN <ahmetgokalppatan@gmail.com>
2024-08-28 17:10:53 +0300

    -INITIAL COMMIT-

---
+++ app/src/main/java/com/patan/commerce/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.patan.commerce
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.patan.commerce.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableEdgeToEdge()
+    }
+}
---
+++ app/src/main/java/com/patan/commerce/ui/HomeFragment.kt
@@ -0,0 +1,15 @@
+package com.patan.commerce.ui
+
+import android.os.Bundle
+import android.view.View
+import com.patan.commerce.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
