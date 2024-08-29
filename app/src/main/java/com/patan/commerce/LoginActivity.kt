da054306ba18755db185b55a66a20acca07b946f
GokalpPATAN <ahmetgokalppatan@gmail.com>
2024-08-29 09:23:52 +0300

    LoginActivity

---
+++ app/src/main/java/com/patan/commerce/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.patan.commerce
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.patan.commerce.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableEdgeToEdge()
+    }
+}
