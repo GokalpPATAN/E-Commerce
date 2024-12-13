cb3971abb4013669943330636a4ff6ed9374e302
GokalpPATAN <ahmetgokalppatan@gmail.com>
2024-12-13 16:54:27 +0300

    Refactors ApiResponse to use correct isSuccess naming.

--- app/src/main/java/com/patan/commerce/model/ApiResponse.kt
+++ app/src/main/java/com/patan/commerce/model/ApiResponse.kt
@@ -1,14 +1,10 @@
 package com.patan.commerce.model
 
-
 import com.google.gson.annotations.SerializedName
 
 data class ApiResponse<T>(
     @SerializedName("Data") val `data`: T?,
     @SerializedName("Message") val message: Any?,
     @SerializedName("MessageSeverity") val messageSeverity: Int?,
-    @SerializedName("IsSuccess") val ısSuccess: Boolean?
+    @SerializedName("IsSuccess") val isSuccess: Boolean?,
 )
-
-
-
